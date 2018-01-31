60016b233bbb4ce42065715613188bb47939a0ad
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-01-31 16:28:14 +0530

    POSITIVE.C

---
+++ POSITIVE.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a;
+ clrscr();
+ printf("ENTER THE NUMBER",a);
+ scanf("%d",&a);
+ if(a>0)
+ printf(" THE NUMBER IS POSITIVE");
+ else
+ printf(" THE NUMBER IS NEGATIVE");
+ getch();
+ return 0;
+ }
