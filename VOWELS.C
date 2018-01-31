e45554e730c6b751c9165c7f382ea6f0cb04b030
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-01-31 16:54:46 +0530

    VOWELS.C

---
+++ VOWELS.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ char b;
+ clrscr();
+ scanf("%c",&b);
+ if(a=a,e,i,o,u)
+ printf("VOWEL");
+ else
+ printf("CONSONANT");
+ getch();
+ return 0;
+ }
