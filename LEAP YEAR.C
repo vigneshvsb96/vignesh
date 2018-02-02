6d28ab80897956d3b1b00ee0c19a97c5dafe3779
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-02 14:04:57 +0530

    LEAP YEAR>C

---
+++ LEAP YEAR.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+  printf("ENTER THE YEAR");
+  scanf("%d",&a);
+  if((a%4)==0)
+  printf("IT IS LEAP YEAR");
+  else
+  printf("IT IS NOT A LEAP YEAR");
+  return 0;
+	
+}
