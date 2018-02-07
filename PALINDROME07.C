7a25b24e0147c876e5f72d317df6ff991ec710a7
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-07 16:13:06 +0530

    PALINDROME07.C

---
+++ PALINDROME07.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+   int a,b,c,d,e;
+   scanf("%d",&a);
+   b=a/100;
+   c=(a-(b*100))/10;
+   d=a-((b*100)+(c*10));
+   e=(d*100)+(c*10)+b;
+   if(a==e)
+   printf("IT iS PALINDROME");
+   else
+   printf("IT IS NOt PALINDROME");
+   return 0;
+}
