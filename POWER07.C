9f545c3a32660d4f7acb01dcb6a6db4e8eb51ee2
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-07 15:46:09 +0530

    POWER07.C

---
+++ POWER07.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("ENTER THE VALUE\n");
+	scanf("%d",&a);
+	printf("ENTER THE vALUE\n");
+	scanf("%d",&b);
+	c=pow(a,b);
+	printf("%d\n",c);
+	return 0;
+}
