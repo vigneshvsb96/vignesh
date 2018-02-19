06182d807c94d993552397a2dd64050d74e3eebf
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-19 15:48:04 +0530

    LARGE19.C

---
+++ LARGE19.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b[3],d,i;
+printf("Enter the number of elements: ");
+scanf("%d",&a);
+printf("Enter the array elements: ");
+for(i=1;i<=a;i++)
+{
+scanf("%d\n",b[i]);
+}
+d=b[0];
+for(i=1;i<=a;i++)
+{
+if(b[i]>d)
+{
+ d=b[i];
+}
+}
+printf("Largest in array is %d",d);
+return 0;
+}
