695c47c4ce79022aac572378968e52d5833f5214
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-21 16:27:43 +0530

    NUMERICAL21.C

---
+++ NUMERICAL21.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char a[100];
+  printf("ENTER THE VALUE");
+  scanf("%s",&a);
+  if(a>='1'&& a<='9')
+  printf("YES");
+  else
+  printf("NO");
+  return 0;
+}
