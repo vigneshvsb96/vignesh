b4f3c7ef399b45b1d47a3adc10a8a805bb851d8b
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-28 16:08:12 +0530

    SWAP28.C

---
+++ SWAP28.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() 
+{
+int x,y;
+printf("\n enter two numbers to swap:");
+scanf("%d %d",&x,&y);
+printf("\n before swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+x=x+y;
+y=x-y;
+x=x-y;
+printf("\n after swapping:");
+printf("\nx=%d",x);
+printf("\ny=%d",y);
+return 0;
+}
