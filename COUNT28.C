d7b21a312fac2701d97c031ff2ecbe34127686cd
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-02-28 15:55:10 +0530

    COUNT28.C

---
+++ COUNT28.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char a[1000];
+	int n,i,count=0;
+	scanf("%[^\t\n]s",a);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	   if(a[i]>='91'&&a[i]<='114')
+	    {
+	        count++;
+	    }
+	    else
+	    {
+	        continue;
+	    }
+	}
+	printf("%d",count);
+	return 0;
+}
