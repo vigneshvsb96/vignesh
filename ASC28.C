13470bfc95e66ef3da0fac3e90917c654be600db
vigneshvsb96 <35764796+vigneshvsb96@users.noreply.github.com>
2018-03-28 03:21:56 -0700

    ASC28.C

---
+++ ASC28.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() 
+{	
+   int n,a[50],i,b=0;
+   scanf("%d",&n);
+   for(i=0;i<n;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<n;i++)
+   {
+        if(a[i]<a[i+1])
+        {
+            b=0;
+        }
+        else
+        {
+            printf("%d",i+1);
+        }
+   }
+	return 0;
+}
